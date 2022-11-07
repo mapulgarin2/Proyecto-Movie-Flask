b45959eb300e4ed1627b3a5ec50c1c80a6c02ae1
unknown <mapulgarin2@gmail.com>
2022-11-06 20:21:57 -0500

    Creo Base de datos

---
+++ doc/base_datos.sql
@@ -0,0 +1,15 @@
+Create table Movie(
+    code varchar(10) PRIMARY KEY,
+    name varchar(100)NOT NULL,
+    image varchar(225),
+    year int(4)
+);
+Create table Review(
+    id int(8)AUTOINCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    description varchar(4000) NOT NULL,
+    rating int(1)NOT NULL,
+    code varchar(10)NOT NULL,
+    CONSTRAINT review_movie_fk FOREIGN KEY (code)  REFERENCES Movie(code)
+);
