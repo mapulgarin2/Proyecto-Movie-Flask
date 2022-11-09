eb0e203b7e81ed9ff6c600f04abf67fc46116d4b
unknown <mapulgarin2@gmail.com>
2022-11-08 23:37:10 -0500

    Se corrige entity.py y se crea repository.py

---
+++ model/repository.py
@@ -0,0 +1,15 @@
+from model.entity import Movie
+
+class MovieRepository:
+    def insert(movie:Movie)->None:
+        pass
+    
+    def findbyCode(code : str)->list:
+        pass
+    
+    def findall()->list:
+        pass
+    
+class ReviewRepository:
+    pass
+        
--- model/repositoryu.py
+++
@@ -1,4 +0,0 @@
-from model.entity import Movie
-class MovieRepository:
-    def insert(movie:Movie)->None:
-        pass
